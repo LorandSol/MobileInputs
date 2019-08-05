d6e8bdf30e171df3919ae2b3f5406dd08bc90d99
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-08-05 12:13:52 +1000

    Rotation

---
+++ Assets/CharacterRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotation : MonoBehaviour
+{
+    public float rotateSpeed = 10f;
+    public Joystick joystickR;
+
+    private void Update()
+    {
+        this.transform.Rotate(new Vector3(0f, 0f, joystickR.Horizontal),rotateSpeed);
+    }
+}
